5a87014365017452bd5f459b5a391e8502c77c98
saglamsa152 <32458548+saglamsa152@users.noreply.github.com>
2018-03-12 20:40:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<a href='ogrencibilgi.php'>Ogrenci Bilgi</a><br>
 		<a href='derssecimi.php'>Ders Secimi</a><br>
 		<a href='bilgilerilistele.php'>Bilgileri Listele</a>
+		selamın aleyk
 	</td>
 </tr>
 </table>
